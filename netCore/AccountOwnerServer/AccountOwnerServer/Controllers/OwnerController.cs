69c657a97e860beb27cd30d12b08cb98d1dc9276
Jason <leminhtam.rp@gmail.com>
2018-04-04 11:47:49 +0800

    get/post api

---
+++ netCore/AccountOwnerServer/AccountOwnerServer/Controllers/OwnerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AccountOwnerServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class OwnerController : Controller
+    {
+        private ILoggerManager _logger;
+        private IRepositoryWrapper _repository;
+
+        public OwnerController(ILoggerManager logger, IRepositoryWrapper repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllOwners()
+        {
+            var owners = _repository.Owner.GetAllOwners();
+            return Ok(owners);
+        }
+    }
+}
--- netCore/AccountOwnerServer/Repository/OwnerRepository.cs
+++ netCore/AccountOwnerServer/Repository/OwnerRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Contracts;
 using Entities;
@@ -12,6 +13,11 @@ namespace Repository
         public OwnerRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
 
+        }
+
+        public IEnumerable<Owner> GetAllOwners()
+        {
+            return FindAll().OrderBy(n => n.Name);
         }
     }
 }
